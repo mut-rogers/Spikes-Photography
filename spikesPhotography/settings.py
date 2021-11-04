9917434dda8f22b3ed18be933f4ab6d7d44ab9c2
rogers <rogersmacharia7@gmail.com>
2021-11-04 14:57:15 +0300

    minor bug fixes

--- spikesPhotography/settings.py
+++ spikesPhotography/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["mjphotography.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
