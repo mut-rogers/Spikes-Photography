c3cab832e6771179453e3ff18fdc094f0773e4c1
rogers <rogersmacharia7@gmail.com>
2021-11-04 13:41:12 +0300

    minor bug fix

--- spikesPhotography/settings.py
+++ spikesPhotography/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mjphotography.herokuapp.com"]
 
 
 # Application definition
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,4 +123,4 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
