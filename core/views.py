5567ad20c0c06f8d25960a20ce653a25d8c62f11
Rogers Macharia <53047712+MACHARIA7@users.noreply.github.com>
2022-08-07 14:31:54 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.views import View
 
 
 class HomepageView(View):
+    """This view renders site's home page"""
 
     def get(self, request):
         return render(self.request, "homepage.html")
